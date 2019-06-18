f3af3e8a9d0c4c23679ddb00eb3120b344197db4
“baoanyu <554287113@qq.com>
2019-06-18 15:37:41 +0800

    add annotions

---
+++ src/main/java/com/example/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,14 @@
+package com.example.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程用来标记线程《不安全》的写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+}
