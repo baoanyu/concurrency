41c5225dea60d0cedb4fdef7075f0b5577030903
“baoanyu <554287113@qq.com>
2019-06-25 08:45:57 +0800

    second

---
+++ src/main/java/com/example/concurrency/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.example.concurrency.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * TestController
+ *
+ * @author bay
+ * @date 2019-06-18-0018 15:39
+ */
+@Controller
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
